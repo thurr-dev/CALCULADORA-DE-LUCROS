b6a59126fd817fa7e9a8dd3b2975564900f1d5ac
thurr-dev <arthurfern89@gmail.com>
2026-02-01 18:40:34 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const cacheName = 'adega-v1';
+const assets = ['./', './index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
